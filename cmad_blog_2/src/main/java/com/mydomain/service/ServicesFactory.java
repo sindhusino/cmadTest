b93dba714122cf42650c47744e8f585b1e19ae97
sindhusino <sindhusino@gmail.com>
2016-06-10 10:14:19 +0530

    Update ServicesFactory.java

--- cmad_blog_2/src/main/java/com/mydomain/service/ServicesFactory.java
+++ cmad_blog_2/src/main/java/com/mydomain/service/ServicesFactory.java
@@ -17,8 +17,8 @@ public class ServicesFactory {
 	 */
 	public static Datastore getMongoDB(){
 		if(mongoTL.get()==null){
-		//	MongoClientURI connectionString = new MongoClientURI("mongodb://173.36.55.70:27017");
-		        MongoClientURI connectionString = new MongoClientURI("mongodb://146.148.47.238:27017");
+			MongoClientURI connectionString = new MongoClientURI("mongodb://173.36.55.70:27017");
+		    //    MongoClientURI connectionString = new MongoClientURI("mongodb://146.148.47.238:27017");
 			MongoClient mongoClient = new MongoClient(connectionString);	
 			Morphia morphia = new Morphia();
 			morphia.mapPackage("com.mydomain.service");
